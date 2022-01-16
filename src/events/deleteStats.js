ccaad147f944a8914f149924dbbacae7edb8ce26
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2022-01-17 00:24:11 +0300

    Star Atmayı Unutmayınız

--- src/events/deleteStats.js
+++ src/events/deleteStats.js
@@ -28,40 +28,34 @@ module.exports = () => {
 
   const daily = new CronJob("0 0 * * *", () => {
     client.guilds.cache.forEach(async (guild) => {
-      guild.members.cache.forEach(async (member) => {
-      await messageGuild.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { dailyStat: 0 } } , { upsert: true });
-      await voiceGuild.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { dailyStat: 0 } } , { upsert: true });
-      await messageUser.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { dailyStat: 0 } } , { upsert: true });
-      await voiceUser.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { dailyStat: 0 } } , { upsert: true });
+      await messageGuild.findOneAndUpdate({ guildID: conf.guildID }, { $set: { dailyStat: 0 } });
+      await voiceGuild.findOneAndUpdate({ guildID: conf.guildID }, { $set: { dailyStat: 0 } });
+      await messageUser.findOneAndUpdate({ guildID: conf.guildID }, { $set: { dailyStat: 0 } });
+      await voiceUser.findOneAndUpdate({ guildID: conf.guildID }, { $set: { dailyStat: 0 } });
           });
-      console.log(`Sunucudaki ${client.guilds.cache.get(conf.guildID).memberCount} üyenin günlük statsları başarıyla sıfırlandı. [00:00]`)
-    });
+
   }, null, true, "Europe/Istanbul");
   daily.start();
 
   const weekly = new CronJob("0 0 * * 0", () => {
     client.guilds.cache.forEach(async (guild) => {
-      guild.members.cache.forEach(async (member) => {
-      await messageGuild.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { weeklyStat: 0 } } , { upsert: true });
-      await voiceGuild.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { weeklyStat: 0 } } , { upsert: true });
-      await messageUser.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { weeklyStat: 0 } } , { upsert: true });
-      await voiceUser.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { weeklyStat: 0 } } , { upsert: true });
+      await messageGuild.findOneAndUpdate({ guildID: conf.guildID }, { $set: { weeklyStat: 0 } });
+      await voiceGuild.findOneAndUpdate({ guildID: conf.guildID }, { $set: { weeklyStat: 0 } });
+      await messageUser.findOneAndUpdate({ guildID: conf.guildID }, { $set: { weeklyStat: 0 } });
+      await voiceUser.findOneAndUpdate({ guildID: conf.guildID }, { $set: { weeklyStat: 0 } });
         });
-      console.log(`Sunucudaki ${client.guilds.cache.get(conf.guildID).memberCount} üyenin haftalık statsı başarıyla sıfırlandı. [00:00]`)
-    });
+
   }, null, true, "Europe/Istanbul");
   weekly.start();
 
   const twoWeekly = new CronJob("0 0 1,15 * *", () => {
     client.guilds.cache.forEach(async (guild) => {
-      guild.members.cache.forEach(async (member) => {
-      await messageGuild.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { twoWeeklyStat: 0 } } , { upsert: true });
-      await voiceGuild.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { twoWeeklyStat: 0 } } , { upsert: true });
-      await messageUser.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { twoWeeklyStat: 0 } } , { upsert: true });
-      await voiceUser.findOneAndUpdate({ guildID: conf.guildID, userID: member.user.id }, { $set: { twoWeeklyStat: 0 } } , { upsert: true });
+      await messageGuild.findOneAndUpdate({ guildID: conf.guildID }, { $set: { twoWeeklyStat: 0 } });
+      await voiceGuild.findOneAndUpdate({ guildID: conf.guildID }, { $set: { twoWeeklyStat: 0 } });
+      await messageUser.findOneAndUpdate({ guildID: conf.guildID }, { $set: { twoWeeklyStat: 0 } });
+      await voiceUser.findOneAndUpdate({ guildID: conf.guildID }, { $set: { twoWeeklyStat: 0 } });
         });
-      console.log(`Sunucudaki ${client.guilds.cache.get(conf.guildID).memberCount} üyenin 2 haftalık statsı başarıyla sıfırlandı. [00:00]`)
-    });
+
   }, null, true, "Europe/Istanbul")
   twoWeekly.start();
 };
