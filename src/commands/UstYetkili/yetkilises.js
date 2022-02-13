07d80514a3cf314e2de55a9172110083db9d7f0b
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2022-02-13 21:27:57 +0300

    Star Atmayı Unutmayınız

--- src/commands/UstYetkili/yetkilises.js
+++ src/commands/UstYetkili/yetkilises.js
@@ -3,6 +3,8 @@ const ayar = require("../../configs/settings.json")
 const moment = require("moment");
 moment.locale("tr");
 const { red } = require("../../configs/emojis.json")
+let table = require("string-table");
+
 module.exports = {
   conf: {
     aliases: ["ysay","yetkilises","sesteolmayan"],
@@ -10,12 +12,42 @@ module.exports = {
     help: "ysay"
   },
 
-  run: async (client, message, args, embed) => {
-    if (!message.member.hasPermission('ADMINISTRATOR')) return message.react(red)
-    let NoVoice = message.guild.members.cache.filter(Tau => Tau.roles.cache.has(ayar.registerPerm)).filter(filterTau => !filterTau.voice.channel&&filterTau.presence.status!="offline")
-    message.channel.send(`
-Aktif olup seste olmayan yetkililer ;
-Seslere geçelim aktifliğiniz ile puanlarınız yükselir ve yetki atlayabilirsiniz.    
-${NoVoice.map(noVoiceMember => `${noVoiceMember}  (${noVoiceMember.user.tag})`).join('\n')}`)
+  run: async (client, message, args, embed, durum) => {
+
+  if (!message.guild) return;
+  if (!message.member.permissions.has(8)) return message.react(red)
+
+    let YetkiliRol = ayar.registerPerm
+    const sec = args[0]
+    if (!sec) {
+
+	  let Ozi = message.guild.members.cache.filter(member => {
+			return member.roles.cache.has(YetkiliRol) && !member.voice.channel && member.presence.status !== "offline" && !member.user.bot && !ayar.owners.includes(member.user.id)
+		  }).map(member => ("<@" + member.user.id + ">")).join(",");
+      let SesteOlmayanYetkili = message.guild.members.cache
+        .filter(member => {
+          return member.roles.cache.has(YetkiliRol) && !member.voice.channel && member.presence.status !== "offline" && !member.user.bot && !ayar.owners.includes(member.user.id)
+        }).size;
+
+      let ToplamYetkili = message.guild.roles.cache.get(YetkiliRol).members.size
+      let SesteOlanYetkili = message.guild.members.cache.filter(member => member.roles.cache.has(YetkiliRol) && member.voice.channel && !member.user.bot && !ayar.owners.includes(member.user.id)).size;
+      let AktifYetkili = message.guild.members.cache.filter(member => member.roles.cache.has(YetkiliRol) && !member.user.bot && !ayar.owners.includes(member.user.id) && (member.presence.status !== "offline")).size;
+      let OfflineYetkili = message.guild.members.cache.filter(member => member.roles.cache.has(YetkiliRol) && !member.user.bot && !ayar.owners.includes(member.user.id) && member.presence.status == "offline").size;
+
+      let tablo = [{
+        "TOPLAM": ToplamYetkili + " kişi",
+        "AKTİF": AktifYetkili + " kişi",
+        "ÇEVRİMDIŞI": OfflineYetkili + " kişi",
+        "SESTE": SesteOlanYetkili + " kişi",
+        "SESTE OLMAYAN": SesteOlmayanYetkili + " kişi"
+      }]
+
+      message.channel.send(table.create(tablo), {
+        code: "md",
+        split: true
+      })
+      message.channel.send(Ozi, {code: "md", split: { char: "," }})
+    }
+
     
 }}
